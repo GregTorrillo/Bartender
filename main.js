c283ddaedaa4e03906dc10c846e2f6197d1f04c8
GregTorrillo <99836116+GregTorrillo@users.noreply.github.com>
2022-04-19 19:16:37 -0400

    Update main.js

--- main.js
+++ main.js
@@ -1,51 +1,29 @@
-//The user will enter a cocktail. Get a cocktail name, photo, and instructions and place them in the DOM
+//The user will enter a cocktail. Get a cocktail name, photo, ingredients, and instructions and place them in the DOM
 
-document.querySelector('button').addEventListener('click', getDrink);
+document.querySelector('button').addEventListener('click',getDrink)
+
 
 function getDrink(){
-    if(document.querySelectorAll('.swiper-slide')!==null){
-        document.querySelectorAll('.swiper-slide').forEach(e => e.remove());
-    }
-    const drink = document.querySelector('input').value;
+  let drink = document.querySelector('input').value
 
-    fetch("https://thecocktaildb.com/api/json/v1/1/search.php?s=" + drink)
-        .then( res=> res.json())
-        .then( data => {
-            console.log(data);
-            if (data.drinks === null){
-                 alert("Sorry! The Bartender is not familiar with that drink, please choose something else.");
-            }
-            else{
-                data.drinks.forEach( cocktail => {
-                    const slide = document.createElement('div');
-                    slide.className = 'swiper-slide';
-                    document.querySelector('.swiper-wrapper').append(slide);
+  fetch(`https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${drink}`)
+      .then(res => res.json()) // parse response as JSON
+      .then(data => {
+        console.log(data.drinks)
+        document.querySelector('h2').innerText = data.drinks[0].strDrink
+        document.querySelector('img').src = data.drinks[0].strDrinkThumb
+        document.querySelector('h3').innerText = data.drinks[0].strInstructions
+        document.querySelector('#ing1').innerText = `${data.drinks[0].strMeasure1} ${data.drinks[0].strIngredient1}`
+        document.querySelector('#ing2').innerText = `${data.drinks[0].strMeasure2} ${data.drinks[0].strIngredient2}`
+        document.querySelector('#ing3').innerText = `${data.drinks[0].strMeasure3} ${data.drinks[0].strIngredient3}`
+        document.querySelector('#ing4').innerText = `${data.drinks[0].strMeasure4} ${data.drinks[0].strIngredient4}`
+        document.querySelector('#ing5').innerText = `${data.drinks[0].strMeasure5} ${data.drinks[0].strIngredient5}`
+        document.querySelector('div').classList.add('hidden')
+        document.querySelector('div').classList.remove('hidden')
+      })
+      .catch(err => {
+          console.log(`error ${err}`)
+      });
 
-                    const card = document.createElement('div');
-                    card.className = 'drinkCard';
-                    slide.appendChild(card);
 
-                    const drinkName = document.createElement('h2');
-                    drinkName.innerText = cocktail.strDrink;
-                    card.appendChild(drinkName);
-
-                    const drinkImage = document.createElement('img');
-                    drinkImage.src = cocktail.strDrinkThumb;
-                    card.appendChild(drinkImage);
-
-                    const instructionsTitle = document.createElement('h3');
-                    instructionsTitle.innerText = "Instructions";
-                    card.appendChild(instructionsTitle);
-
-                    const drinkInstructions = document.createElement('p');
-                    drinkInstructions.innerText = cocktail.strInstructions.replace(/(\n|\r|\n\r)/gm,'');
-                    card.appendChild(drinkInstructions);
-                });
-            }
-        })
-        .catch(err => {
-            console.log(`error ${err}`)
-        });
-
-    // document.querySelector('.swiper-slide').hidden = false;
 }
